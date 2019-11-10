e3ceb4e67d2c9d10ad2d66c8ef343d784a10b95c
David Beales <David.Beales@ukho.gov.uk>
2019-11-10 10:19:00 +0000

    update logic

--- calendarRestService/calendarRepoAzure.js
+++ calendarRestService/calendarRepoAzure.js
@@ -1,16 +1,15 @@
 var storage = require('azure-storage');
 var config = require('./config.js');
-var storageClient;
 
-module.exports = function () {
+module.exports =
+class CalendarRepo {
+    constructor() {
+        this.storageClient = storage.createTableService(config.storageAccount, config.storageKey);
+    }
 
-    this.setup = function () {
-        storageClient = storage.createTableService(config.storageAccount, config.storageKey);
-    };
-
-    this.getCalendarRecords = function () {
+    getCalendarRecords() {
         return new Promise(function (res, rej) {
-            storageClient.createTableIfNotExists(config.storageTableLive, function (error, createResult) {
+            this.storageClient.createTableIfNotExists(config.storageTableLive, function (error, createResult) {
                 if (error) rej();
                 if (createResult.isSuccessful) {
                     console.log(`1. Create Table operation executed successfully for: ${config.storageTableLive}\n`);
@@ -22,14 +21,31 @@ module.exports = function () {
         }.bind(this)).then(function (result) {
             return new Promise((res) => { res(result); });
         });
-    };
+    }
 
-    this.getRecords = function () {
+    getRecordsForYearAndMonth(year, month) {
         return new Promise(function (res, rej) {
             let query = new storage.TableQuery();
-            storageClient.queryEntities(config.storageTableLive, query, null, function (error, result, response) {
+            this.storageClient.queryEntities(config.storageTableLive, query, null, function (error, result, response) {
                 if (error) rej(error);
-                console.log(`2. Ran query operation executed successfully`);
+                let recordsForMonthAndYear = []
+                const results = this.parseResults(result.entries);
+                for (let i = 0; i < results.length; i++) {
+                    const record = results[i];
+                    const date = new Date(Number.parseInt(record.date))
+                    if (date.getFullYear() === year && date.getMonth() === month)
+                        recordsForMonthAndYear.push(record);
+                }
+                res(JSON.stringify(recordsForMonthAndYear));
+            }.bind(this));
+        }.bind(this));
+    }
+
+    getRecords() {
+        return new Promise(function (res, rej) {
+            let query = new storage.TableQuery();
+            this.storageClient.queryEntities(config.storageTableLive, query, null, function (error, result, response) {
+                if (error) rej(error);
                 //var orderResultsByTime = result.entries.sort(function (a, b) {
                 //    return a['date'] === b['date'] ? 0 : a['date'] > b['date'] ? 1 : -1;
                 //});
@@ -37,24 +53,24 @@ module.exports = function () {
                 res(JSON.stringify(results));
             }.bind(this));
         }.bind(this));
-    };
+    }
 
-    this.parseResults = function (entries) {
+    parseResults(entries) {
         let jsonArray = [];
         for (let i = 0; i < entries.length; i++)
             jsonArray.push(this.parseResult(entries[i]));
         return jsonArray;
-    };
+    }
 
-    this.parseResult = function (entry) {
+    parseResult(entry) {
         return {
             id: entry.RowKey._,
             title: entry.title._,
             date: entry.date._
         };
-    };
+    }
 
-    this.addCalendarRecord = function (jsonRecord) {
+    addCalendarRecord(jsonRecord) {
         const invertedTimeKey = this.getMaxTimeTicks() - jsonRecord.date;
         let entity = {
             PartitionKey: `${invertedTimeKey}`,
@@ -63,24 +79,24 @@ module.exports = function () {
             date: jsonRecord.date
         };
         return this.updateOrReplaceRecord(entity);
-    };
+    }
 
-    this.getMaxTimeTicks = function () {
+    getMaxTimeTicks() {
         return (new Date(9999, 12, 31, 23, 59, 59, 9999999)).getTime();
-    };
+    }
 
-    this.updateOrReplaceRecord = function (entity) {
+    updateOrReplaceRecord(entity) {
         return new Promise(function (res, rej) {
-            storageClient.insertOrReplaceEntity(config.storageTableLive, entity, function (error, result, response) {
+            this.storageClient.insertOrReplaceEntity(config.storageTableLive, entity, function (error, result, response) {
                 if (!error) {
                     res(entity.RowKey);
                 }
                 rej();
             }.bind(this));
         }.bind(this));
-    };
+    }
 
-    this.updateCalendarRecord = function (jsonRecord) {
+    updateCalendarRecord(jsonRecord) {
         var entity = {
             PartitionKey: jsonRecord.id,
             RowKey: jsonRecord.id,
@@ -89,12 +105,12 @@ module.exports = function () {
             date: jsonRecord.date
         };
         return this.updateOrReplaceRecord(entity);
-    };
+    }
 
-    this.deleteCalendarRecord = function (id) {
+    deleteCalendarRecord(id) {
         return new Promise(function (res, rej) {
             this.getRecordById(id).then(function (retrievedEntity) {
-                storageClient.deleteEntity(config.storageTableLive, retrievedEntity, function entitiesQueried(error, result) {
+                this.storageClient.deleteEntity(config.storageTableLive, retrievedEntity, function entitiesQueried(error, result) {
                     if (error) {
                         rej();
                     }
@@ -102,14 +118,40 @@ module.exports = function () {
                 });
             });
         }.bind(this));
-    };
+    }
 
-    this.getRecordById = function (id) {
+    getRecordById(id) {
         return new Promise(function (res, rej) {
-            storageClient.retrieveEntity(config.storageTableLive, id, id, function (error, result, response) {
+            this.storageClient.retrieveEntity(config.storageTableLive, id, id, function (error, result, response) {
                 if (error) rej(error);
                 res(result);
             }.bind(this));
         }.bind(this));
-    };
-};
+    }
+
+    calendarRecordHasValidId(json) {
+        return json.hasOwnProperty('id') && this.isString(json.id) && this.isNotEmptyString(json.id);
+    }
+
+    isNotEmptyString(value) {
+        return this.isString(value) && value.trim() !== "";
+    }
+
+    isString(value) {
+        return Object.prototype.toString.call(value) === '[object String]';
+    }
+
+    isNumber(value) {
+        return Object.prototype.toString.call(value) === '[object Number]';
+    }
+
+    isValidCalendarRecord(json) {
+        jsonProperties = ['title', 'date'];
+        for (let i = 0; i < jsonProperties.length; i++) {
+            const key = jsonProperties[i];
+            if (!json.hasOwnProperty(key)) return false;
+            else if (json[key].length < 1 || json[key].length > 20) return false;
+        }
+        return true;
+    }
+}
--- calendarRestService/server.js
+++ calendarRestService/server.js
@@ -1,29 +1,76 @@
-var http = require('http');
-var url = require('url');
-var cs = require('./calendarService');
-var calendarService = new cs();
-var port = 8080;
+var cr = require('./calendarRepoAzure');
+var calendarRepo = new cr();
 
-http.createServer(function (request, response) {
-    response.setHeader('Access-Control-Allow-Origin', '*');
-    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE');
-    response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+var port = process.env.PORT || 8080;
+var express = require('express');
+var app = express();
 
-    if (request.method.toLowerCase() === 'get') {
-        calendarService.getCalendarRecords(response);
+var allowCrossDomain = function (request, response, next) {
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    response.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+};
+
+app.use(allowCrossDomain);
+app.use(express.json());
+
+app.get('/events', function (request, response) {
+    let month = Number(request.param('month'));
+    let year = Number(request.param('year'));
+    if (!calendarRepo.isNumber(month) || !calendarRepo.isNumber(year)) {
+        return response.status(400).send('Month and year are not numbers.');
     }
 
-    else if (request.method.toLowerCase() === 'post') {
-        calendarService.addOrUpdateCalendarRecord(request, response);
-    }
+    calendarRepo.getRecordsForYearAndMonth(year, month)
+        .then((results) => {
+            return response.status(201).json(results);
+        })
+        .catch((err) => {
+            console.error(err);
+            return response.status(400).send('Get calendar records failed.');
+        });
+});
 
-    else if (request.method.toLowerCase() === 'delete') {
-        var id = url.parse(request.url, true).query.id;
-        calendarService.deleteCalendarRecord(id, response);
-    }
 
-    else {
-        response.write(`rest type not supported`);
-        response.end();
-    }
-}).listen(process.env.PORT || port);
+app.post('/', function (request, response) {
+    let json = request.body;
+    calendarRepo.addCalendarRecord(json)
+        .then((id) => {
+            return response.status(201).json(`posted data: ${id}`);
+        })
+        .catch((err) => {
+            console.error(err.stack);
+            return response.status(400).send('Post calendar event failed.');
+        });
+});
+
+app.put('/', function (request, response) {
+    let json = request.body;
+    calendarRepo.updateCalendarRecord(json)
+        .then((id) => {
+            return response.status(201).json(`posted data: ${json.id}`);
+        })
+        .catch((err) => {
+            console.error(err.stack);
+            return response.status(400).send('Post calendar event failed.');
+        });
+});
+
+app.delete('/', function (request, response) {
+    const id = request.param('id');
+    calendarRepo.deleteCalendarRecord(id)
+        .then((id) => {
+            return response.status(201).json(`deleted record: ${id}.`);
+        })
+        .catch((err) => {
+            console.error(err.stack);
+            return response.status(400).send('Delete calendar event failed.');
+        });
+});
+
+var server = app.listen(port, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("Example app listening at http://%s:%s", host, port);
+});
